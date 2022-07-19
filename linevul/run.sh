ccdea0e2cc25f65926ba7bb9fadc227e689e0d89
satyaki-das-usc <satyakid@usc.edu>
2022-07-19 15:01:30 -0700

    do train

--- linevul/run.sh
+++ linevul/run.sh
@@ -21,8 +21,6 @@ PYTHONPATH="." python linevul_main.py \
     --eval_batch_size 256 \
     --write_raw_preds \
     --do_sorting_by_line_scores \
-    --do_sorting_by_pred_prob \
-    --do_local_explanation
 
 # PYTHONPATH="." python linevul_main.py \
 #     --model_name=12heads_linevul_model.bin \
@@ -35,3 +33,18 @@ PYTHONPATH="." python linevul_main.py \
 #     --block_size 512 \
 #     --eval_batch_size 256 \
 #     --write_raw_preds \
+
+
+PYTHONPATH="." python linevul_main.py \
+    --model_name=12heads_linevul_model.bin \
+    --output_dir=./saved_models \
+    --model_type=roberta \
+    --tokenizer_name=microsoft/codebert-base \
+    --model_name_or_path=microsoft/codebert-base \
+    --do_test \
+    --test_data_file=../data/zero_day/zero_day_vul_only.csv \
+    --block_size 512 \
+    --eval_batch_size 256 \
+    --write_raw_preds \
+    --do_train \
+    --train_data_file=../data/zero_day/chromium_free.csv
