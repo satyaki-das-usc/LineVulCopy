cb881046208cd760b9bef8fe61b13c77587cbbbb
satyaki-das-usc <satyakid@usc.edu>
2022-08-22 16:01:59 -0700

    line level prediction and training

--- linevul/run.sh
+++ linevul/run.sh
@@ -35,23 +35,6 @@
 #     --eval_batch_size 256 \
 #     --write_raw_preds \
 
-# PYTHONPATH="." python linevul_main.py \
-#     --model_name=12heads_linevul_model.bin \
-#     --output_dir=./saved_models \
-#     --model_type=roberta \
-#     --tokenizer_name=microsoft/codebert-base \
-#     --model_name_or_path=microsoft/codebert-base \
-#     --do_test \
-#     --test_data_file=../data/devign/devign_test.csv \
-#     --block_size 512 \
-#     --eval_batch_size 256 \
-#     --write_raw_preds \
-#     --do_train \
-#     --train_data_file=../data/devign/devign_train.csv \
-#     --eval_data_file=../data/devign/devign_val.csv \
-#     --do_sorting_by_line_scores \
-#     --reasoning_method=attention
-
 PYTHONPATH="." python linevul_main.py \
     --model_name=12heads_linevul_model.bin \
     --output_dir=./saved_models \
@@ -66,3 +49,20 @@ PYTHONPATH="." python linevul_main.py \
     --do_train \
     --train_data_file=../data/devign/devign_train.csv \
     --eval_data_file=../data/devign/devign_val.csv \
+    --do_sorting_by_line_scores \
+    --reasoning_method=attention
+
+# PYTHONPATH="." python linevul_main.py \
+#     --model_name=12heads_linevul_model.bin \
+#     --output_dir=./saved_models \
+#     --model_type=roberta \
+#     --tokenizer_name=microsoft/codebert-base \
+#     --model_name_or_path=microsoft/codebert-base \
+#     --do_test \
+#     --test_data_file=../data/devign/devign_test.csv \
+#     --block_size 512 \
+#     --eval_batch_size 256 \
+#     --write_raw_preds \
+#     --do_train \
+#     --train_data_file=../data/devign/devign_train.csv \
+#     --eval_data_file=../data/devign/devign_val.csv \
