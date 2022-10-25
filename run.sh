ff273dfd6c8ddc6eece88f19535dad638eefc526
satyaki-das-usc <satyakid@usc.edu>
2022-10-25 12:47:51 -0700

    changed run config

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 PYTHONPATH="." python data_visualizer.py
 
-PYTHONPATH="." python categorical_dataset_generator.py --dataset-type train
-PYTHONPATH="." python categorical_dataset_generator.py --dataset-type eval
+PYTHONPATH="." python categorical_dataset_generator.py --dataset-type train &&
+PYTHONPATH="." python categorical_dataset_generator.py --dataset-type eval &&
 PYTHONPATH="." python categorical_dataset_generator.py --dataset-type test
 
 rm devign_test.csv devign_train.csv devign_val.csv
