b676d0918e31036dc986a25c1bcc39bc3f9428c3
Tuan Ngo <tuanngokien@gmail.com>
2023-09-04 14:36:04 -0700

    backward compability for transformer lib while loading model with load_state_dict

--- linevul/linevul_main.py
+++ linevul/linevul_main.py
@@ -1306,7 +1306,7 @@ def main():
     if args.do_test:
         checkpoint_prefix = f'checkpoint-best-f1/{args.model_name}'
         output_dir = os.path.join(args.output_dir, '{}'.format(checkpoint_prefix))  
-        model.load_state_dict(torch.load(output_dir, map_location=args.device, strict=False))
+        model.load_state_dict(torch.load(output_dir, map_location=args.device), strict=False)
         model.to(args.device)
         test_dataset = TextDataset(tokenizer, args, file_type='test')
         test(args, model, tokenizer, test_dataset, curr_timestamp, best_threshold=0.5)
